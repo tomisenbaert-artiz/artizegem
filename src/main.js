e0459519205c78fe81858df6f30561a97384c021
tomisenbaert-artiz <tom.isenbaert@artiz-izegem.be>
2025-10-21 16:45:55 +0200

    added background

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import './style.css'
+const aanbodButton = document.getElementById('aanbod-button');
+const aanbodDropdown = document.getElementById('aanbod-dropdown');
+
+// Toggle dropdown on button click
+aanbodButton.addEventListener('click', () => {
+aanbodDropdown.classList.toggle('hidden');
+});
+
+// Close dropdown if clicked outside
+window.addEventListener('click', (event) => {
+if (!aanbodButton.contains(event.target) && !aanbodDropdown.contains(event.target)) {
+    aanbodDropdown.classList.add('hidden');
+ }
+ });
